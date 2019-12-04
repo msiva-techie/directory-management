3f4a8bc9001c76feaebf1b70149fdce8d1988a0a
Siva M <siva.m@ramco.com>
2019-12-04 20:24:41 +0530

    initial commit

---
+++ routes/directory.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+function getFiles(source){
+	let files = fs.readdirSync(source), 
+		result = [];
+	for(file of files){
+		let dirName = `${source}/${file}`;
+		if(fs.statSync(dirName).isDirectory()){
+			result = [...getFiles(dirName)];
+		}else{
+			result.push(dirName.toLowerCase());
+		}
+	}
+	return result;
+}
+
+router.post('/compare', function compare(req, res) {	
+try{
+	let fromDirectoryFiles = getFiles(req.body.fromDirectory);
+	let toDirectoryFiles = getFiles(req.body.toDirectory);
+
+	let result = [], f;
+	for(let i=0; i < fromDirectoryFiles.length; i++){
+		f=0;
+		for(let j=0; j < toDirectoryFiles.length; j++){
+			if(fromDirectoryFiles[i].substring(req.body.fromDirectory.length) 
+				=== toDirectoryFiles[j].substring(req.body.toDirectory.length)){
+					f=1;
+			}
+		}
+		if(f===0){
+			result.push(fromDirectoryFiles[i]);
+		}
+	}
+	res.jsonp({
+		additionalFiles:result
+	});
+} catch(err) {
+	res.jsonp({
+		message:'some error occured',
+		err
+	});
+}
+});
+
+router.post('/delete', function compare(req, res) {	
+	try{
+		req.body.files.forEach((file) => {
+			fs.unlinkSync(file);
+		})
+		res.jsonp({
+			message:'Files deleted'
+		});
+	} catch(err) {
+		res.jsonp({
+			message:'some error occured',
+			err
+		});
+	}
+	});
+
+module.exports = router;
